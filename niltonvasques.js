e1e4ce2d97489b502a0d1b72c1b6defe80400661
Nilton Vasques <nilton.vasques@gmail.com>
2015-06-24 18:06:04 -0300

    Improve portfolio.

--- niltonvasques.js
+++ niltonvasques.js
@@ -2,6 +2,19 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  Template.body.helpers({
+    skills : [
+      { image_url: "images/android.png", name: "Android", 
+        description: "Possuo conhecimentos avançados no desenvolvimento de aplicativos para a plataforma android, com mais de 4 anos de experiência. Com mais de 1 milhão de downloads obtidos com os aplicativos próprios publicados no Google Play.", image: true},
+      { image_url: "images/rails.png", name: "Ruby on Rails", 
+        description: "Conhecimentos básicos no framework Ruby on Rails, no qual continuo em constante processo de aprimoramento.", image: true},
+      { image_url: "images/nodejs.png", name: "NodeJS", 
+        description: "We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.", image: true},
+      { image_url: "images/meteor.png", name: "Meteor", 
+        description: "We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.", image: true}
+    ]
+  });
+
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
